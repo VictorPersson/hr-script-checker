71d84db76342890ebfee4dbfda0cb6d78010a8c1
VictorPersson <vp-96@hotmail.com>
2022-05-10 00:04:32 +0200

    Refactor folder strucutre, basic popup design, logic

---
+++ basics/js/popup.js
@@ -0,0 +1,11 @@
+console.log("Content script is loaded");
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? "from a content script:" + sender.tab.url
+      : "from the extension"
+  );
+  console.log(request);
+  sendResponse({ responseBackground: "Response to background" });
+});
