26a4b964ee666f036360eaaf0f4baf8ba23283bc
VictorPersson <vp-96@hotmail.com>
2022-05-14 21:18:11 +0200

    General clean up, add fix for script check tooltip, script linking

--- basics/js/popup.js
+++ basics/js/popup.js
@@ -1,37 +1,16 @@
 console.log("Content script is loaded");
 
 const modHtml = (scriptActive) => {
-  console.log(scriptActive);
-  const infoTitle = document.getElementById("main-header");
+  const infoTitle = document.querySelector("#hr-extension-main #main-header");
+
+  console.log(infoTitle, scriptActive);
 
   infoTitle.innerText = `This site is ${
     scriptActive ? "" : "not"
   } running Hello Retail ${scriptActive ? "✔️" : "❌"}`;
 };
 
-const buildPopup = (status) => {
-  scriptActive = status;
-  if (location.hash == "#popup") {
-    console.log(status);
-    const infoTitle = document.getElementById("main-header");
-    console.log(infoTitle);
-
-    infoTitle.innerText = `This site is ${
-      status ? "" : "not"
-    } running Hello Retail ${status ? "✔️" : "❌"}`;
-  }
-};
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  setScriptStatus(request.scriptActive).then(
-    sendResponse({ responseBackground: "Response to background" })
-  );
-  return true;
-});
-
 chrome.storage.local.get(["scriptActive"], function (result) {
-  console.log("RES:", result.scriptActive);
-
   const scriptActive = result.scriptActive.statusCode === 200;
   modHtml(scriptActive);
 });
